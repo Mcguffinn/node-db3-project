e128372df6a7d0eb16a4e52e48bb0ac80ad7cd96
Mcguffinn <deronviledwin@gmail.com>
2020-05-14 16:01:11 -0400

    reached code mvp

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,44 +1,39 @@
 const db = require("../data/dbConfig.js");
 
 module.exports = {
-    find,
-    findById,
-    findSteps,
-    add,
-    update,
-    remove,
-}
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove,
+};
 
 function find() {
-    return db("scheme");
+  return db("schemes");
 }
 
 function findById(id) {
-    return db("scheme").where({id}).first();
+  return db("schemes").where({ id }).first();
 }
 
-//lambda style function 
+//lambda style function
 function findSteps(id) {
-    return db("scheme")
-    .where({id})    ///.first(); <---- may not need just the #1
+  return db("schemes").where({ id }); ///.first(); <---- may not need just the #1
 }
 
 function add(scheme) {
-    return db("scheme")
+  return db("schemes")
     .insert(scheme, "id")
-    .then(ids =>{
-        return findById(ids[0]);
+    .then((ids) => {
+      return findById(ids[0]);
     });
 }
 
-function update(id, changes) {
-    return db("users")
-    .where({id})
-    .update(changes);
+function update(changes, id) {
+  return db("schemes").where({ id }).update(changes);
 }
 
 function remove(id) {
-    return db("users").where({id})
-    .del()
+  return db("schemes").where({ id }).del();
 }
-
